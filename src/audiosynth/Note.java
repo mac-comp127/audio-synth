270c2d947d03720a149242ff799b68a40dc66f29
Paul Cantrell <cantrell@pobox.com>
2019-11-17 23:10:21 -0600

    Note equality

--- src/audiosynth/Note.java
+++ src/audiosynth/Note.java
@@ -2,12 +2,14 @@ package audiosynth;
 
 import audiosynth.instrument.Instrument;
 
+import java.util.Objects;
+
 public final class Note {
     private final Instrument instrument;
     private final double pitch, startTime, duration;
 
     public Note(Instrument instrument, double pitch, double startTime, double duration) {
-        this.instrument = instrument;
+        this.instrument = Objects.requireNonNull(instrument, "instrument");
         this.pitch = pitch;
         this.startTime = startTime;
         this.duration = duration;
@@ -31,5 +33,25 @@ public final class Note {
 
     public double getDuration() {
         return duration;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null || getClass() != that.getClass()) {
+            return false;
+        }
+        Note note = (Note) that;
+        return Double.compare(note.pitch, pitch) == 0 &&
+            Double.compare(note.startTime, startTime) == 0 &&
+            Double.compare(note.duration, duration) == 0 &&
+            instrument.equals(note.instrument);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(instrument, pitch, startTime, duration);
     }
 }

eff2e8a08ea724f4f36ffc14db9c49e3156031a9
Paul Cantrell <cantrell@pobox.com>
2019-11-17 23:09:51 -0600

    Moved main into separate class

---
+++ src/audiosynth/AudioSynth.java
@@ -0,0 +1,27 @@
+package audiosynth;
+
+import comp127graphics.CanvasWindow;
+
+import java.awt.Color;
+import java.util.Arrays;
+
+public class AudioSynth {
+    public static void main(String[] args) {
+        String fileName = "kondo.csv";
+        Song song = new SongReader().readSong(fileName);
+
+        SongVisualization visualization = new SongVisualization(80, 6);
+        visualization.showSong(song);
+
+        CanvasWindow window = new CanvasWindow(
+            fileName,
+            (int) visualization.getWidth(),
+            (int) visualization.getHeight());
+        window.add(visualization);
+        window.setBackground(Color.BLACK);
+
+        song.renderAudio().play(
+            visualization::setTime,
+            () -> System.out.println("Done!"));
+    }
+}
--- src/audiosynth/SongReader.java
+++ src/audiosynth/SongReader.java
@@ -1,7 +1,6 @@
 package audiosynth;
 
 import audiosynth.instrument.*;
-import comp127graphics.CanvasWindow;
 
 import java.awt.Color;
 import java.io.InputStream;
@@ -42,24 +41,5 @@ public class SongReader {
             song.addNote(new Note(instrument, pitch, startTime, duration));
         }
         return song;
-    }
-
-    public static void main(String[] args) {
-        String fileName = "kondo.csv";
-        Song song = new SongReader().readSong(fileName);
-
-        SongVisualization visualization = new SongVisualization(80, 6);
-        visualization.showSong(song);
-
-        CanvasWindow window = new CanvasWindow(
-            fileName,
-            (int) visualization.getWidth(),
-            (int) visualization.getHeight());
-        window.add(visualization);
-        window.setBackground(Color.BLACK);
-
-        song.renderAudio().play(
-            visualization::setTime,
-            () -> System.out.println("Done!"));
     }
 }

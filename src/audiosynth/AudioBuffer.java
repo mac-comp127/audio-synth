8d806db302bfea5cd927895cc4cbd4ef64874107
Paul Cantrell <cantrell@pobox.com>
2019-11-19 21:40:54 -0600

    mix()

--- src/audiosynth/AudioBuffer.java
+++ src/audiosynth/AudioBuffer.java
@@ -72,22 +72,6 @@ public class AudioBuffer {
     }
 
     /**
-     * Renders a slice of the given signal into this audio buffer, adding the new signal to the
-     * sample values already present in the buffer.
-     *
-     * @param signal    A sound source.
-     * @param offset    The number of samples into the audio buffer to start adding the new signal.
-     *                  This offset is time 0 for the signal.
-     * @param duration  The number of samples of the signal to add to the buffer. The offset +
-     *                  duration must not exceed the length of the buffer.
-     */
-    public void mix(Signal signal, int offset, int duration) {
-        for(int n = 0; n < duration && n + offset < samples.length; n++) {
-            samples[n + offset] += signal.amplitudeAt(n);
-        }
-    }
-
-    /**
      * Plays this buffer to the system's default sound output. This method is _asynchronous_: it
      * does not wait for playback to finish.
      */

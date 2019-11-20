0e7a16ee849774ef9ca4ded05b50a7ce905a2c27
Paul Cantrell <cantrell@pobox.com>
2019-11-19 19:20:47 -0600

    SongVisualization

--- src/audiosynth/AudioSynth.java
+++ src/audiosynth/AudioSynth.java
@@ -11,17 +11,6 @@ public class AudioSynth {
     public static void main(String[] args) {
         String fileName = "kondo.csv";
         Song song = new SongReader().readSong(fileName);
-
-        SongVisualization visualization = new SongVisualization(80, 6);
-        visualization.showSong(song);
-
-        CanvasWindow window = new CanvasWindow(
-            fileName,
-            (int) visualization.getWidth(),
-            (int) visualization.getHeight());
-        window.add(visualization);
-        window.setBackground(Color.BLACK);
-
         song.renderAudio().play();
     }
 }

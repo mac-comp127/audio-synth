66007069bff4ee2da517461ce34e17a52242ee9e
Paul Cantrell <cantrell@pobox.com>
2020-10-09 17:22:50 -0500

    SongReader

--- src/audiosynth/AudioSynth.java
+++ src/audiosynth/AudioSynth.java
@@ -9,9 +9,18 @@ import audiosynth.waveform.TriangularWave;
  */
 public class AudioSynth {
     public static void main(String[] args) {
+        // 3-note test song
+
         Song song = new Song();
         song.addNote(new Note(new SineWave(), 51, 0, 2.0));
         song.addNote(new Note(new TriangularWave(), 58, 0.5, 1.5));
         song.addNote(new Note(new SquareWave(), 67, 1.0, 1.0));
+
+        // Longer test song
+
+        // String fileName = "kondo.csv";
+        // Song song = new SongReader().readSong(fileName);
+
+        // song.renderAudio().play();
     }
 }
---
+++ src/audiosynth/SongReader.java
@@ -0,0 +1,68 @@
+package audiosynth;
+
+import audiosynth.waveform.*;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * A utility for reading Songs from text files. The text file consists of the following four values
+ * in this order:
+ *
+ * - One of the following waveform names: sine tri square saw noise
+ * - pitch in MIDI units
+ * - start time in seconds
+ * - duration in seconds
+ *
+ * ...repeated for every note in the song. Values may be separated by commas, spaces, or newlines.
+ * The order of notes does not matter.
+ */
+public class SongReader {
+    private final Map<String, Waveform> waveforms = new HashMap<>();
+
+    /**
+     * Creates a new song reader with the following name-to-waveform mappings:
+     *
+     * - "sine"   → SineWave
+     * - "tri"    → TriangularWave
+     * - "square" → SquareWave
+     * - "saw"    → SawtoothWave
+     * - "noise"  → Noise
+     */
+    public SongReader() {
+        waveforms.put("sine", new SineWave());
+        waveforms.put("tri", new TriangularWave());
+        waveforms.put("square", new SquareWave());
+        waveforms.put("saw", new SawtoothWave());
+        waveforms.put("noise", new Noise());
+    }
+
+    /**
+     * Reads song data from the given project resource.
+     */
+    public Song readSong(String resourceName) {
+        return readSong(getClass().getResourceAsStream("/" + resourceName));
+    }
+
+    /**
+     * Reads song data from an arbitrary input stream.
+     */
+    @SuppressWarnings("resource")
+    public Song readSong(InputStream in) {
+        Scanner scanner = new Scanner(in).useDelimiter("\\s*,\\s*|\\s+");
+        Song song = new Song();
+        while(scanner.hasNext()) {
+            String waveformName = scanner.next();
+            Waveform waveform = Objects.requireNonNull(
+                waveforms.get(waveformName), "No waveform named " + waveformName);
+            double pitch = scanner.nextDouble();
+            double startTime = scanner.nextDouble();
+            double duration = scanner.nextDouble();
+            song.addNote(new Note(waveform, pitch, startTime, duration));
+        }
+        return song;
+    }
+}
